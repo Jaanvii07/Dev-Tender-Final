84ab18df56a8d6714ed29dbaa91c95757955832e
Janvi Patidar <janvilakhanpatidar@gmail.com>
2026-05-20 12:17:30 +0530

    Allow VITE_API_URL

--- frontend/my-app/src/utils/constent.js
+++ frontend/my-app/src/utils/constent.js
@@ -1,3 +1,3 @@
-// If VITE_BASE_URL is not set, fallback to your deployed backend URL.
+// If VITE_BASE_URL or VITE_API_URL is not set, fallback to your deployed backend URL.
 // When testing locally, it uses localhost. 
-export const BASE_URL = import.meta.env.VITE_BASE_URL || (location.hostname === "localhost" ? "http://localhost:3000" : "/api");
+export const BASE_URL = import.meta.env.VITE_BASE_URL || import.meta.env.VITE_API_URL || (location.hostname === "localhost" ? "http://localhost:3000" : "/api");
